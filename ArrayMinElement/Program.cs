0e5e5c4ae6929857eacfe6173da57e9ee848ff12
Igor <ihor.kaseko@aploris.com>
2022-04-23 21:08:04 +0300

    Add project files.

---
+++ ArrayMinElement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ArrayMinElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("How many numbers do you want to store in array: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int[] numbers = new int[n];
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write("Enter number {0}:  ", i + 1);
+                        numbers[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    int min = numbers[0];
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (min > numbers[i])
+                        {
+                            min = numbers[i];
+                        }
+                    }
+                    Console.WriteLine("The minimum is: {0}", min);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
